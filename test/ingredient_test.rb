0d75cbe68928376453d41d7949898d4bf6b90f81
Ben Fox <benfox1216@gmail.com>
2020-01-08 10:05:47 -0700

    Set up basic test file for ingredient class

---
+++ test/ingredient_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+
+class IngredientTest < Minitest::Test
+  def setup
+    
+  end
+  
+  def test_it_exists
+    
+  end
