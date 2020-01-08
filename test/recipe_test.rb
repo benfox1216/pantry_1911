a9f559aabe141ec496c17ad841f4dfc84449f0dc
Ben Fox <benfox1216@gmail.com>
2020-01-08 10:20:09 -0700

    Add new attribute ingredients_required, with passing test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -1,10 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/recipe'
+require './lib/ingredient'
 
 class RecipeTest < Minitest::Test
   def setup
     @recipe = Recipe.new("Mac and Cheese")
+    
+    @ingredient1 = Ingredient.new("Cheese", "C", 100)
+    @ingredient2 = Ingredient.new("Macaroni", "oz", 30)
   end
   
   def test_it_exists
@@ -12,6 +16,9 @@ class RecipeTest < Minitest::Test
   end
   
   def test_it_has_attributes
+    expected = {}
+    
     assert_equal "Mac and Cheese", @recipe.name
+    assert_equal expected, @recipe.ingredients_required
   end
 end
