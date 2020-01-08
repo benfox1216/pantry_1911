5e352af7d29b32168b55624e2dbfb2e2c43200a5
Ben Fox <benfox1216@gmail.com>
2020-01-08 10:09:26 -0700

    Create ingredient class, with initialize working

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -4,9 +4,10 @@ require './lib/ingredient'
 
 class IngredientTest < Minitest::Test
   def setup
-    
+    @ingredient = Ingredient.new("Cheese", "oz", 50)
   end
   
   def test_it_exists
     
   end
+end
