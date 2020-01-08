7c946cb892bf935051eaaaf7a6ba42133b1212b2
Ben Fox <benfox1216@gmail.com>
2020-01-08 10:12:50 -0700

    Add passing test "test_it_has_attributes"

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -10,4 +10,10 @@ class IngredientTest < Minitest::Test
   def test_it_exists
     assert_instance_of Ingredient, @ingredient
   end
+  
+  def test_it_has_attributes
+    assert_equal "Cheese", @ingredient.name
+    assert_equal "oz", @ingredient.unit
+    assert_equal 50, @ingredient.calories
+  end
 end
