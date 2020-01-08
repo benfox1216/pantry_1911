d6714ccc9389c32da5edcd7022ddef5f7d186b4d
Ben Fox <benfox1216@gmail.com>
2020-01-08 10:50:43 -0700

    Set up pantry test

---
+++ test/pantry_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/ingredient'
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @recipe = Recipe.new("Mac and Cheese")
+    
+    @ingredient1 = Ingredient.new("Cheese", "C", 100)
+    @ingredient2 = Ingredient.new("Macaroni", "oz", 30)
+  end
+  
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+  
+  def test_it_has_attributes
+    
+  end
+end
