8fa815d0b7c90c989d13b516ec1d309a7e55e704
Ben Fox <benfox1216@gmail.com>
2020-01-08 12:01:08 -0700

    Add final commits, Iteration 4 almost finished

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,2 +1,11 @@
 class CookBook
+  attr_reader :summary
+  
+  def initialize
+    @summary = []
+  end
+  
+  def add_recipe(recipe)
+    @summary << recipe
+  end
 end
--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -1,18 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/cook_book'
-require './lib/pantry'
 require './lib/recipe'
 require './lib/ingredient'
 
 class CookBookTest < Minitest::Test
   def setup
     @cookbook = CookBook.new
-    @pantry = Pantry.new
-    @recipe = Recipe.new("Mac and Cheese")
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Burger")
     
     @ingredient1 = Ingredient.new("Cheese", "C", 100)
     @ingredient2 = Ingredient.new("Macaroni", "oz", 30)
+    @ingredient3 = Ingredient.new("Ground Beef", "oz", 100)
+    @ingredient4 = Ingredient.new("Bun", "g", 1)
+    
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 100)
   end
   
   def test_it_exists
@@ -20,6 +27,14 @@ class CookBookTest < Minitest::Test
   end
   
   def test_it_has_attributes
+    assert_equal [], @cookbook.summary
+  end
+  
+  def test_it_can_add_recipes_to_summary
     skip
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe1)
+    
+    #assert_equal @cookbook.summary
   end
 end
